bbebf743d9ed4611bcc8e40d98814192cf923e15
az-m <alisonzmills@gmail.com>
2025-07-09 15:24:59 +0100

    feat: added tabs to profile page

--- src/app/profile/[userId]/page.jsx
+++ src/app/profile/[userId]/page.jsx
@@ -2,8 +2,9 @@ import { auth } from "@/auth";
 import { db } from "@/db";
 import { notFound } from "next/navigation";
 import { UserPostList } from "@/components/UserPostList";
+import Link from "next/link";
 
-export default async function Profile({ params }) {
+export default async function Profile({ params, searchParams }) {
   const session = await auth();
   const thisProfile = parseInt((await params).userId);
 
@@ -11,9 +12,29 @@ export default async function Profile({ params }) {
     notFound();
   }
 
+  const query = await searchParams;
+  console.log(query);
+  let posts, comments;
+
+  if (Object.keys(query).length === 0) {
+    posts = true;
+  } else {
+    posts = query.posts;
+  }
+
+  comments = query.comments;
+
   return (
     <>
-      <UserPostList userId={session.user.id} />
+      <div className="flex justify-center gap-4 py-5 text-2xl">
+        <Link href={`?posts=true`} className={posts && `text-pink-500`}>
+          My Posts
+        </Link>
+        <Link href={`?comments=true`} className={comments && `text-pink-500`}>
+          My Comments
+        </Link>
+      </div>
+      {posts && <UserPostList userId={session.user.id} />}
     </>
   );
 }
